9f90d3edb6fecf66baa044205a404cc3259a843b
deepikamaddali <deepikamaddali17@gmail.com>
2017-05-12 15:12:42 +0530

    first commit

--- src/main/java/com/internship/assignment/App.java
+++ src/main/java/com/internship/assignment/App.java
@@ -1,38 +1,81 @@
 package com.internship.assignment;
 import java.sql.*;
+import java.util.*;
+
 class sqltest
 {
-	public static void main(String args[])
-	{
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "admin");
-			if(Integer.parseInt(args[0]) ==1 )
+private static Scanner sc;
+
+public static void main(String args[])throws Exception
+{
+		int ch = 0;
+		boolean flag=true;
+		
+		
+		try
+		{
+			
+		Class.forName("com.mysql.jdbc.Driver");
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "admin");
+			
+			
+			do
 			{
-				System.out.println("accepts 1");
-			}
-				else if (Integer.parseInt(args[0]) == 2)
+			System.out.println("enter 1 for query 1/tenter two for query 2/tenter 3 for query 3/tenter 4 for query 4/n");
+			
+			 sc = new Scanner(System.in);
+ 
+		switch (ch)
 			{
-		System.out.println("accepts 2");
-			} 
-			else
+			
+			case 1 :
+			try
 			{
-				System.out.println("does not accept");
-			}
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from emp order by salary desc limit 5");
+				Statement stmt1 = c.createStatement();
+			ResultSet rs = stmt1.executeQuery("select * from emp order by salary desc limit 5");
 			System.out.println("-----query 1----");
-			while (rs.next()) {
+			while (rs.next())
+			{
 				System.out.println(rs.getInt(1) + "   " + rs.getString(2) + "   " + rs.getString(3) + "  "
 						+ rs.getString(4) + "  " + rs.getInt(5) + "  " + rs.getInt(6));
 				// System.out.println(rs.getInt(1));
 			}
+		   
+			}
+			 
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+			
+			break ;
+			
+			
+			case 2:
+			try
+			{
+				Statement stmt = c.createStatement();
+			
 			ResultSet rs1 = stmt.executeQuery("select * from emp group by joining_date desc,designation desc limit 1");
 			System.out.println("-----query 2----");
-			while (rs1.next()) {
+			while (rs1.next())
+			{
 				System.out.println(rs1.getInt(1) + "   " + rs1.getString(2) + "   " + rs1.getString(3) + "  "
 						+ rs1.getString(4) + "  " + rs1.getInt(5) + "  " + rs1.getInt(6));
-              }
+			}
+			
+			}
+		 catch (Exception e)
+			{
+				System.out.println(e);
+			}
+			 break ;
+			 
+			 case 3:
+			 try
+			 {
+				 Statement stmt = c.createStatement();
+			 
 			ResultSet rs2 = stmt.executeQuery(" select * from emp group by designation order by business_unit; ");
 			System.out.println("-----query 3----");
 			while (rs2.next())
@@ -40,16 +83,45 @@ class sqltest
 				//System.out.println(rs2.getInt(1));
 				System.out.println(rs2.getInt(1) + "   " + rs2.getString(2) + "   " + rs2.getString(3) + "  "+ rs2.getString(4) + "  " + rs2.getInt(5) + "  " + rs2.getInt(6));
 			}
+			
+			 }catch (Exception e)
+			{
+				System.out.println(e);
+			}
+			 break ;
+			 
+			case 4:
+			try
+			{
+				Statement stmt = c.createStatement();
+			
 			ResultSet rs3 = stmt.executeQuery(" select * from emp group by designation order by joining_date desc limit 5 ");
 			System.out.println("-----query 4----");
 			while (rs3.next())
 			{
 			System.out.println(rs3.getInt(1) + "   " + rs3.getString(2) + "   " + rs3.getString(3) + "  "+ rs3.getString(4) + "  " + rs3.getInt(5) + "  " + rs3.getInt(6));
 			}
-			con.close();
-		 }catch (Exception e)
+			c.close();
+		 }
+			catch (Exception e)
 		{
 			System.out.println(e);
 		}
-	}
-}
+			
+		break;
+			}
+			System.out.println("do you want to continue?(y/n");
+		
+		String str=sc.nextLine();
+	 if(str.equals("y"))
+		 flag=true;
+    if(str.equals("n"))
+		flag=false;
+			}
+while(flag);
+		}
+		finally 
+		{
+			System.out.println("executed");
+		}
+}}
